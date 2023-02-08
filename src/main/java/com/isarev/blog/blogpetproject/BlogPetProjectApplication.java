949b0a8cdc881910539dd40285bff4452ac0f614
vladsw764 <vladsw764@icloud.com>
2023-02-08 11:49:06 +0200

    Initial commit

---
+++ src/main/java/com/isarev/blog/blogpetproject/BlogPetProjectApplication.java
@@ -0,0 +1,13 @@
+package com.isarev.blog.blogpetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPetProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPetProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isarev/blog/blogpetproject/BlogPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isarev.blog.blogpetproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPetProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
