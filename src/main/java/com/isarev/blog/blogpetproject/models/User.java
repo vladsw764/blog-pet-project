ed1a0915b62513fa0ec96d740fb39b5fa54b26d8
vladsw764 <vladsw764@icloud.com>
2023-03-12 22:08:23 +0200

    add User Model

---
+++ src/main/java/com/isarev/blog/blogpetproject/models/User.java
@@ -0,0 +1,76 @@
+package com.isarev.blog.blogpetproject.models;
+
+import com.isarev.blog.blogpetproject.enums.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "users")
+@NoArgsConstructor
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "email", unique = true)
+    private String email;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password", length = 1000)
+    private String password;
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role",
+    joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles = new HashSet<>();
+
+    public User(String email, String phoneNumber, String username, String password) {
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.username = username;
+        this.password = password;
+    }
+
+    //security
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
